dace4fbe19d9e3d4bfd927d4828b8225b160529b
= <57084177+fuurinko@users.noreply.github.com>
2022-03-20 10:56:59 +0800

    Site updated: 2022-03-20 10:56:58

---
+++ dist/music.js
@@ -0,0 +1,12 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+	{
+        name: "平凡之路",
+        artist: '朴树',
+        url: 'http://www.ytmp3.cn/down/59211.mp3',
+        cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
+      },
+});
